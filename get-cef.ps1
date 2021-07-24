93cc603567e2ac216ab688e89dcef3f394c3df6b
Maurice Heumann <maurice.heumann@wibu.com>
2021-07-24 16:36:08 +0200

    Update CEF

--- get-cef.ps1
+++ get-cef.ps1
@@ -1,4 +1,4 @@
-$cefVersion = 'cef_binary_91.1.22+gc67b5dd+chromium-91.0.4472.124_windows64'
+$cefVersion = 'cef_binary_91.1.23+g04c8d56+chromium-91.0.4472.164_windows64'
 
 #-------------------------------------------------
 Write-Output 'Deleting old CEF binaries...'
