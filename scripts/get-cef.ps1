c3f3d64b23998decbb34d971e57b9de91af92a7c
Jan <jan@laupetin.net>
2021-09-25 15:52:08 +0200

    Automatically install CEF via premake

--- scripts/get-cef.ps1
+++ scripts/get-cef.ps1
@@ -1,4 +1,9 @@
-$cefVersion = 'cef_binary_92.0.25+gd15cfa8+chromium-92.0.4515.131_windows64'
+if ($args.Count -lt 1) {
+    Write-Output "Usage: <cef-version>"
+    exit 1
+}
+
+$cefVersion = $args[0]
 
 #-------------------------------------------------
 Write-Output 'Deleting old CEF binaries...'
@@ -13,6 +18,59 @@ Remove-Item $destination -ErrorAction Ignore
 Remove-Item -LiteralPath $cefPath -Force -Recurse -ErrorAction Ignore
 
 #-------------------------------------------------
+Write-Output 'Locating 7Zip'
+#-------------------------------------------------
+
+function Get-SzRegistry {
+    $reg = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+
+    if ($null -eq $reg) {
+        return $null
+    }
+
+    $szKey = $reg.OpenSubKey('SOFTWARE')
+    if ($null -eq $szKey) {
+        return $null
+    }
+
+    $szKey = $szKey.OpenSubKey('7-Zip')
+    if ($null -eq $szKey) {
+        return $null
+    }
+
+    return $szKey.GetValue('Path') + '7z.exe'
+}
+
+function Get-SzDefaultPaths {
+    $path = "$env:ProgramFiles\7-Zip\7z.exe"
+
+    if(-Not(Test-Path -Path $path -PathType Leaf)) {
+        $path = "$env:ProgramW6432\7-Zip\7z.exe"
+    }
+
+    if(-Not(Test-Path -Path $path -PathType Leaf)) {
+        $path = "${env:ProgramFiles(x86)}\7-Zip\7z.exe"
+    }
+
+    if(-Not(Test-Path -Path $path -PathType Leaf)) {
+        return $null
+    }
+
+    return $path
+}
+
+$sz = Get-SzRegistry
+
+if ($null -eq $sz) {
+    $sz = Get-SzDefaultPaths
+}
+
+if ($null -eq $sz) {
+    Write-Error -Message 'Could not locate 7Zip. Make sure you have 7Zip installed and try again.'
+    exit 1
+}
+
+#-------------------------------------------------
 Write-Output 'Downloading CEF...'
 #-------------------------------------------------
 
@@ -22,14 +80,12 @@ $cefURLVersion = [System.Web.HttpUtility]::UrlEncode($cefVersion)
 $source = "https://cef-builds.spotifycdn.com/$cefURLVersion.tar.bz2"
 (New-Object Net.WebClient).DownloadFile($source, $destination)
 
-$sz = '"C:\Program Files\7-Zip\7z.exe"'
-
 #-------------------------------------------------
 Write-Output 'Unpacking CEF...'
 #-------------------------------------------------
 
-cmd /c "$sz x $destination -aoa"
-cmd /c "$sz x $destinationPart -aoa"
+& "$sz" x $destination -aoa
+& "$sz" x $destinationPart -aoa
 
 Move-Item -Path $cefVersion -Destination $cefPath
 
